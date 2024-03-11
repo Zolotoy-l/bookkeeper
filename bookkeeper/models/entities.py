81d9c69116ad6e15374b5562edee445b8a8536cc
Лев <l.zolotoy@yandex.ru>
2024-03-11 13:20:15 +0300

    installed pony and added project entities file

---
+++ bookkeeper/models/entities.py
@@ -0,0 +1,32 @@
+from datetime import date
+from pony.orm import *
+
+
+db = Database()
+
+
+class Budget(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    moonthly = Required(float)
+    weekly = Required(float)
+    daily = Required(float)
+
+
+class Expense(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    amount = Required(int)
+    expense_date = Required(date)
+    added_date = Required(date)
+    comment = Optional(str)
+    category = Required('Category')
+
+
+class Category(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    expenses = Set(Expense)
+    parent = Optional('Category', reverse='parent')
+
+
+
+db.generate_mapping()
